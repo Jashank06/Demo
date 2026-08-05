cc23b05a57edd36bdbd6365121451464e3576eeb
Md Motiullah <motiullahrxl@gmail.com>
2026-08-05 22:51:01 +0530

    Added my name

--- test.py
+++ test.py
@@ -3,10 +3,8 @@ print("Demo")
 print("hello")
 print("Sandeep")
 print("Raghav")
-<<<<<<< HEAD
 print("piyush")
-=======
 print("Mohammad Raza")
 print("Devraj")
 print(" raghav......")
->>>>>>> 84eac1b72b29e08f23a6e6179da8046c5c0434d0
+print("Md Motiullah")
